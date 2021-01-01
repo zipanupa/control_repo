148651ffb3c41b3ff4f82890854eab85df7ec261
zipanupa <zipanupa@gmail.com>
2021-01-01 14:52:09 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,14 @@ node default{
      owner   => 'root',
     }
   }
-  node 'master.puppet.vm' {
+ node 'master.puppet.vm' {
     include role::master_server
-  }
+ }
+
+node /^web/ {
+    include role::app_server
+}
+
+node /^db/ {
+    include role::db_server
+}
