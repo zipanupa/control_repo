f4aa1dcc8fb3f581488883102a3612839167ad41
zipanupa <zipanupa@gmail.com>
2021-01-02 10:36:45 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCyn5spJ1blzVmsfmMhHmsnOwDmPGxkCnU6FuMOsqwHn8Ods73+GKUDJ884lljMLso8PrA40wgJU6N3ViTnRGsi3Rax4Ow6y+q92afFv3jG8QtGTi2FPQvlQbUpUrgLCIsXi5l4bXpuWi4I34UJa6Ju1ZEZKGgEsfyhZxXf9dusGrewJz1uNv5Vf5NVLRRpmC5DHM4HVnFtiNmS7raaFgkORXx93CSucCkUFtaG813REA7Kw5MlUgnKNjhRQOI7ME/p2iW2n2ajDdkQ0Ab0gSzTHXceKKvD2mfdbLhWqNLJRLhY5zMBqqTwbfbStUckvvAom9WdD8qYXCaKHz7wJFyb',
   }  
 }
