ef1d82cb0c9708d4718f6dd4ad8a6e3e66bdfdd5
zipanupa <zipanupa@gmail.com>
2021-01-02 10:55:52 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,6 +3,7 @@ class profile::agent_nodes{
   include dockeragent
   dockeragent::node{'web.puppet.vm':}
   dockeragent::node{'db.puppet.vm':}
+  dockeragent::node{'minetest.puppet.vm':}
   
   host {'web.puppet.vm':
       ensure => present,
