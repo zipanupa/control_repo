4831fdb4eefd435908166974be43b083b9fba01f
zipanupa <zipanupa@gmail.com>
2021-01-02 11:25:04 +0000

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft{
+    file {'/opt/minecraft':
+        ensure => directory,
+    }
+    file {'/opt/minecraft/minecraft_server.jar':
+        ensure => file,
+        source => 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
+    }
+    package {'java':
+        ensure => present,
+    }
+    file {'/opt/minecraft/eula.txt':
+        ensure  => file,
+        content => 'eula=true',
+    }
+    file {'/etc/systemd/system/minecraft.service':
+        ensure => file,
+        source => 'puppet:///modules/minecraft/minecraft.service',
+    }
+}
