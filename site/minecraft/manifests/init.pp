78df66a0c0817125e94765f49363b18d1d8f9a85
zipanupa <zipanupa@gmail.com>
2021-01-02 16:59:20 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,18 @@
 class minecraft{
+    $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar'
+    $install_dir = '/opt/minecraft' 
     file {'/opt/minecraft':
         ensure => directory,
     }
-    file {'/opt/minecraft/minecraft_server.jar':
+    file {"${install_dir}/minecraft_server.jar":
         ensure => file,
-        source => 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
+        source => $url,
+        before => Service['minecraft'],
     }
     package {'java':
         ensure => present,
     }
-    file {'/opt/minecraft/eula.txt':
+    file {"${install_dir}/eula.txt":
         ensure  => file,
         content => 'eula=true',
     }
@@ -20,5 +23,6 @@ class minecraft{
     service {'minecraft':
         ensure => running,
         enable => true,
+        require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
     }
 }
