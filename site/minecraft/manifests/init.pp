c8d4380eac1b3ba96fb27460c171b50ae1f444b7
zipanupa <zipanupa@gmail.com>
2021-01-02 17:10:45 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,8 +19,10 @@ class minecraft{
     }
     file {'/etc/systemd/system/minecraft.service':
         ensure => file,
-        source => 'puppet:///modules/minecraft/minecraft.service',
-    }
+        content=> epp('minecraft/minecraft.service',{ 
+            install_dir => $install_dir,
+        })
+        }
     service {'minecraft':
         ensure => running,
         enable => true,
