682d5dec061aa062e8d1ce2d221978fc97e1b941
zipanupa <zipanupa@gmail.com>
2021-01-02 11:27:52 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,4 +17,7 @@ class minecraft{
         ensure => file,
         source => 'puppet:///modules/minecraft/minecraft.service',
     }
+    service {'minecraft':
+        ensure => running,
+        enable => true,
 }
